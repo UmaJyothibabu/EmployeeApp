32c13f8ed99651b4e446ae063dcd9d1dc65fd615
Uma K J <umakj1995@gmail.com>
2023-07-20 14:18:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "/build")));
 
 require("dotenv").config();
-require("./Db/connect");
+// require("./Db/connect");
 
 PORT = process.env.PORT || 5000;
 
@@ -20,19 +20,33 @@ app.use("/api", employeeRouter);
 
 const userRouter = require("./Routes/userRoute");
 app.use("/api", userRouter);
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.mongodb_url);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "/build/index.html"));
 });
 
-mongoose
-  .connect(process.env.mongodb_url)
-  .then(() => {
-    console.log("Connected to atlas");
-    app.listen(PORT, () => {
-      console.log(`The server is running on port ${PORT}`);
-    });
-  })
-  .catch(() => {
-    console.log("Error!! DB Connection lost");
+// mongoose
+//   .connect(process.env.mongodb_url)
+//   .then(() => {
+//     console.log("Connected to atlas");
+//     app.listen(PORT, () => {
+//       console.log(`The server is running on port ${PORT}`);
+//     });
+//   })
+// .catch(() => {
+//   console.log("Error!! DB Connection lost");
+// });
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("listening for requests");
   });
+});
